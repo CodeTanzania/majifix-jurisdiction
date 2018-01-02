fba1f84e2e859b830d3a806c5094f2359290f68b
lykmapipo <lallyelias87@gmail.com>
2018-01-02 11:45:43 +0300

    initialize

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @module starter
+ * @version 0.1.0
+ * @description starter module for 311 modules
+ * @author lally elias <lallyelias87@gmail.com>
+ * @public
+ */
+
+module.exports = {
+  name: 'starter'
+};
