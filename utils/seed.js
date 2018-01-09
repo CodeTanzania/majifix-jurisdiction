c1b5a454b4c0441da94a4159006e3bed16db7146
Benson Maruchu <benmaruchu@gmail.com>
2018-01-09 10:01:59 +0300

    add documentation in seed function utility function

--- utils/seed.js
+++ utils/seed.js
@@ -1,9 +1,16 @@
 'use strict';
 
 /**
+ * This function seed  a collection of jurisdictions provided to it as the first
+ *  argument.
  * @function
+ * @param {Array|Object} jurisdictions - Jurisdictions collection or object to seed
+ * @param {Function} done - Callback when the function finished seeding data
+ * @version 0.1.0
+ * @since 0.1.0
  */
 
+//  dependencies
 const path = require('path');
 const _ = require('lodash');
 const Jurisdiction = require(path.join(__dirname, '..', 'models',
