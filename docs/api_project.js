b733fa3df6892f07a1ef9647337b82a69af72eaf
Benson Maruchu <benmaruchu@gmail.com>
2018-05-23 14:43:29 +0300

    generate apidoc

---
+++ docs/api_project.js
@@ -0,0 +1,20 @@
+define({
+  "name": "majifix-jurisdiction",
+  "version": "1.0.0",
+  "description": "A representation of an entity (e.g municipal) responsible for addressing citizen(or customer) service request(issue).",
+  "title": "majifix - Jurisdiction",
+  "url": "https://majifix-jurisdiction.herokuapp.com/v1",
+  "sampleUrl": "https://majifix-jurisdiction.herokuapp.com/v1",
+  "template": {
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-05-23T11:42:06.429Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
