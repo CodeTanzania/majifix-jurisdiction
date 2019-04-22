7ab66b6426c64c6906269f0178ee44c597ade71f
rijkerd <richardaggrey7@gmail.com>
2019-04-22 15:07:04 +0300

    chore: remove mongoose from bootstrap spec

--- test/bootstrap.spec.js
+++ test/bootstrap.spec.js
@@ -3,12 +3,3 @@
 /* force environment to be test */
 process.env.NODE_ENV = 'test';
 process.env.DEFAULT_LOCALE = 'en';
-
-/* setup mongoose */
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-
-/* setup */
-require('chai').use(require('sinon-chai'));
-require('sinon');
-require('sinon-mongoose');
