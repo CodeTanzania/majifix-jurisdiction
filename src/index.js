4bcd1261d5fbeffef12a3fa456c3530857fbd84f
rijkerd <richardaggrey7@gmail.com>
2019-04-23 19:35:10 +0300

    refactor: change package import and destruct field from package.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-'use strict';
-
-
 /**
  * @name majifix-jurisdiction
  * @description A representation of an entity (e.g municipal)
@@ -23,67 +20,40 @@
  *
  */
 
-
 /* dependencies */
-const path = require('path');
-const _ = require('lodash');
-const app = require('@lykmapipo/express-common');
-
+import app from '@lykmapipo/express-common';
+import {
+  name,
+  description,
+  version,
+  license,
+  homepage,
+  repository,
+  bugs,
+  sandbox,
+  contributors,
+} from '../package.json';
+import Jurisdiction from './jurisdiction.model';
+import router from './http.router';
 
 /* declarations */
-const pkg = require(path.join(__dirname, 'package.json'));
-const fields = [
-  'name',
-  'description',
-  'version',
-  'license',
-  'homepage',
-  'repository',
-  'bugs',
-  'sandbox',
-  'contributors'
-];
-
-
-/* extract information from package.json */
-const info = _.merge({}, _.pick(pkg, fields));
-
-
-/* import models */
-const Jurisdiction =
-  require(path.join(__dirname, 'lib', 'jurisdiction.model'));
-
-
-/* import routers*/
-const router =
-  require(path.join(__dirname, 'lib', 'http.router'));
-
-
-/* export package(module) info */
-exports.info = info;
-
-
-/* export jurisdiction model */
-exports.Jurisdiction = Jurisdiction;
-
-
-/* export jurisdiction router */
-exports.router = router;
-
+const info = {
+  name,
+  description,
+  version,
+  license,
+  homepage,
+  repository,
+  bugs,
+  sandbox,
+  contributors,
+};
 
 /* export router api version */
-exports.apiVersion = router.version;
+const apiVersion = router.version;
 
+/*bind jurisdiction router */
+app.mount(router);
 
-/* export app */
-Object.defineProperty(exports, 'app', {
-  get() {
-
-    //TODO bind oauth middlewares authenticate, token, authorize
-
-    /* bind jurisdiction router */
-    app.mount(router);
-    return app;
-  }
-
-});
+/* export */
+export { app, apiVersion, info, Jurisdiction, router };
