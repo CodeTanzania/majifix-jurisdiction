c55239acddaefebc8158661081fed45738f1c93a
lykmapipo <lallyelias87@gmail.com>
2019-08-28 18:10:01 +0300

    refactor(examples): improve sample seed & app

--- examples/app.js
+++ examples/app.js
@@ -1,71 +1,35 @@
-/* ensure mongo uri */
-process.env.MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://localhost/majifix-jurisdiction';
-
-/* dependencies */
-const _ = require('lodash');
-const async = require('async');
-const { app, mount, start } = require('@lykmapipo/express-common');
+const { get, mount, start } = require('@lykmapipo/express-common');
 const { connect, jsonSchema } = require('@lykmapipo/mongoose-common');
-// mongoose.set('debug', true);
-const { Jurisdiction, apiVersion, info, router } = require('../lib/index');
-let samples = require('./samples')(20);
+const { jurisdictionRouter, info, apiVersion } = require('../lib');
 
-/* connect to mongoose */
-mount(router);
+const startHttpServer = () => {
+  get('/', (request, response) => {
+    response.status(200);
+    response.json(info);
+  });
 
-connect(connectionError => {
-  if (connectionError) {
-    throw connectionError;
-  }
-  async.waterfall(
-    [
-      function clear(next) {
-        Jurisdiction.remove(() => {
-          next();
-        });
-      },
+  get(`/${apiVersion}/schemas`, (request, response) => {
+    const schema = jsonSchema();
+    response.status(200);
+    response.json(schema);
+  });
 
-      function seedJurisdiction(next) {
-        const jurisdiction = Jurisdiction.fake();
-        jurisdiction.post(next);
-      },
+  // mount routers
+  mount(jurisdictionRouter);
 
-      function seed(jurisdiction, next) {
-        /* fake jurisdictions */
-        samples = _.map(samples, (sample, index) => {
-          const data = sample;
-          if (index % 2 === 0) {
-            data.jurisdiction = jurisdiction;
-          }
-          return data;
-        });
-        Jurisdiction.create(samples, next);
-      },
-    ],
-    function serve(asyncError) {
-      if (asyncError) {
-        throw asyncError;
-      }
-      /* expose module info */
-      app.get('/', (request, response) => {
-        response.status(200);
-        response.json(info);
-      });
-
-      app.get(`/${apiVersion}/schemas`, (request, response) => {
-        const schema = jsonSchema();
-        response.status(200);
-        response.json(schema);
-      });
-
-      /* fire the app */
-      start((error, env) => {
-        // eslint-disable-next-line no-console
-        console.log(
-          `visit http://0.0.0.0:${env.PORT}/${apiVersion}/jurisdictions`
-        );
-      });
+  // fire http serve
+  start((error, env) => {
+    if (error) {
+      throw error;
     }
-  );
+    console.log(`visit http://0.0.0.0:${env.PORT}/${apiVersion}/jurisdictions`);
+  });
+};
+
+// connect and start http server
+connect(error => {
+  if (error) {
+    throw error;
+  }
+  startHttpServer();
 });
---
+++ examples/seed.js
@@ -0,0 +1,48 @@
+const _ = require('lodash');
+const { waterfall } = require('async');
+const { connect, clear } = require('@lykmapipo/mongoose-common');
+const { Jurisdiction } = require('../lib');
+
+/* track seeding time */
+let seedStart;
+let seedEnd;
+
+const log = (stage, error, results) => {
+  if (error) {
+    console.error(`${stage} seed error`, error);
+  }
+
+  if (results) {
+    const val = _.isArray(results) ? results.length : results;
+    console.info(`${stage} seed result`, val);
+  }
+};
+
+const clearSeed = next => clear(Jurisdiction, () => next());
+
+const seedJurisdiction = next => {
+  const jurisdictions = Jurisdiction.fake(50);
+  Jurisdiction.create(jurisdictions, next);
+};
+
+const seed = () => {
+  seedEnd = Date.now();
+  waterfall([clearSeed, seedJurisdiction], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    seedEnd = Date.now();
+
+    log('time', null, seedEnd - seedStart);
+    log('final', error, results);
+    process.exit(0);
+  });
+};
+
+// connect and seed
+connect(error => {
+  if (error) {
+    throw error;
+  }
+  seed();
+});
